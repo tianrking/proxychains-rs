4b1d51439b471a512ef703f6a531976c8567b5c0
tianrking <10758833+tianrking@users.noreply.github.com>
2026-09-17 05:03:39 +0800

    Revert "feat(unix): track UDP sessions across fcntl duplication"
    This reverts commit 76b6777d21dec53b06ec0095424dd0cefec07699.

--- libproxychains/src/macos_interpose.rs
+++ libproxychains/src/macos_interpose.rs
@@ -5,7 +5,6 @@ use std::ffi::c_void;
 
 extern "C" {
     fn gethostbyname(name: *const libc::c_char) -> *mut libc::hostent;
-    fn proxychains_fcntl(fd: libc::c_int, command: libc::c_int, ...) -> libc::c_int;
 }
 
 #[repr(C)]
@@ -27,7 +26,7 @@ macro_rules! interpose {
 
 #[used]
 #[link_section = "__DATA,__interpose,interposing"]
-static INTERPOSE: [Interpose; 18] = [
+static INTERPOSE: [Interpose; 17] = [
     interpose!(unix_impl, connect),
     interpose!(unix_impl, getaddrinfo),
     interpose!(unix_impl, freeaddrinfo),
@@ -43,10 +42,6 @@ static INTERPOSE: [Interpose; 18] = [
     interpose!(udp_exports, read),
     interpose!(udp_exports, write),
     interpose!(udp_exports, close),
-    Interpose {
-        replacement: proxychains_fcntl as *const () as *const c_void,
-        original: libc::fcntl as *const () as *const c_void,
-    },
     interpose!(udp_exports, getpeername),
     interpose!(udp_exports, sendmsg),
     interpose!(udp_exports, recvmsg),
